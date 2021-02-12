80f9b02496b94d688eb41eff375689667ac4ae9d
pragya53 <pragyashashwati5@gmail.com>
2021-02-12 21:57:50 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -3,10 +3,10 @@ import clx.xms
 import requests
 
 def sendSMS(number1,number2,name):
-    client = clx.xms.Client(service_plan_id='ba94aad89039417c9fcd303555432171', token='12c806b724f64fa78552b0c84b4ff009')
+    client = clx.xms.Client(service_plan_id=', token='')
 
     create = clx.xms.api.MtBatchTextSmsCreate()
-    create.sender = '447537404817'
+    create.sender = ''
     create.recipients = {'91'+number1,'91'+number2}
     create.body = 'HELP!!!!!!! '+name+' is in an emergency and needs your help.'
 
