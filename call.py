c83d859486838cb2da127fbc2a6fc5d919d66090
pragya53 <pragyashashwati5@gmail.com>
2021-02-12 21:56:58 +0530

    Update call.py

--- call.py
+++ call.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'AC9690fad23b011b612a217d6449059138'
-auth_token = 'ec31d0ed3ab66e23e4e7850bebbd26dc'
+account_sid = ''
+auth_token = ''
 
 def sos_call(phone_number):
     client = Client(account_sid, auth_token)
@@ -10,7 +10,7 @@ def sos_call(phone_number):
 
     call = client.calls.create(
         url='http://demo.twilio.com/docs/voice.xml',
-        to='+15558675310',
-        from_='+15017122661'
+        to='',
+        from_=''
     )
     print(call.sid)
